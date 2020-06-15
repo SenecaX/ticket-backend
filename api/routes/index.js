f9ccf7e2a07ae612caa3f7872f3b90bc0f5f27c5
Kedaren Dorasami <kedaren.dorasami@elca.mu>
2020-06-15 20:30:27 +0400

    apis

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,29 +2,8 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/task");
 
-//task
-// router
-//   .route("/task")
-//   .get(taskController.taskId)
-//   .post(taskController.taskCreate);
-
-// router
-//   .route("/task/:userId")
-//   .get(taskController.taskReadOne)
-//   .put(taskController.taskUpdateOne)
-//   .delete(taskController.taskDeleteOne);
-
-// router
-//   .route("/task")
-//   .get(taskController.taskId)
-//   .post(taskController.taskCreate);
-
-// router
-//   .route("/task/:userId")
-//   .get(taskController.taskListByUserId)
-//   .post(taskController.taskCreate);
-//   .delete(taskController.taskDeleteOne);
-
 router.route("/task").post(taskController.taskCreate);
+
+router.route("/task/:userId").get(taskController.taskListByUserId);
 
 module.exports = router;
